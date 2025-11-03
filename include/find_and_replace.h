1782813433cd8031fe0171b491803a9e65457072
hrujul15 <hrujulmendhe2024@gmail.com>
2025-11-04 01:20:58 +0530

    Resolve search.h name conflict and include issues

--- include/find_and_replace.h
+++ include/find_and_replace.h
@@ -1,16 +1,16 @@
-#ifndef SEARCH_H
-#define SEARCH_H
-
+#ifndef FIND_AND_REPLACE_H
+#define FIND_AND_REPLACE_H
 #include <string>
-
+#include <cstddef>
+// convert string to lowercase for case-insensitive search
+std::string toLower(const std::string &s);
 // Find all occurrences of a given word in a file (case-insensitive)
 void findWordInFile(const std::string &filePath, const std::string &word);
 
 // Find and display the most frequent word in the file (case-insensitive)
 void findMostFrequentWord(const std::string &filePath);
 
-//Find and replace feature(case-sensitive)
+// Find and replace feature(case-sensitive)
 void findAndReplaceInFile(const std::string &filePath, const std::string &target, const std::string &replacement);
 
-
-#endif 
+#endif
